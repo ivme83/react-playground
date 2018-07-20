392695c3bde94a8f2e657ee086c3ae9c23a8c8c8
Roland Willis <rolandwillisiv@gmail.com>
2018-07-20 11:23:24 -0700

    set up babel and live-server

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  'Hello There'
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,5 @@
+const template = <p>Hello There</p>;
+
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
